275eec314df85fcfaa2afd60f518a2e3c9045877
chaodeng <645111208@qq.com>
2018-10-12 14:17:20 +0800

    up

--- asset/37. Sudoku Solver/1.Sudoku Solver.py
+++ asset/37. Sudoku Solver/1.Sudoku Solver.py
@@ -20,52 +20,104 @@ class Point:
         self.x = x
         self.x = y
         self.available = []
-        self.value = '.'
+        self.value = 0
 
 
 class Solution:
 
     # 创建新的数独库
     def __init__(self):
-        self.Sudoku = [[0]*9 for i in range(9)]
+        # self.Sudoku = [[0]*9 for i in range(9)]
+        self.Sudoku = []
         self.board = []
 
-    def getRowAvailableNums(self, y):
-        return [[range(9)].remove(n) for n in self.board[y]]
+    # 获取行记录
+    def getRowNums(self, y):
+        return self.board[y]
 
-    def getColAvailableNums(self, x):
-        return [[range(9)].remove(n[x]) for n in self.board]
+    # 获取列记录
+    def getColNums(self, x):
+        return [n[x] for n in self.board]
 
-    def getCubeAvailableNums(self, x, y):
+    # 获取九宫格记录
+    def getCubeNums(self, x, y):
         return 0
-        pass
+
+    def selfCount(self):
+        for y, row in enumerate(self.board):
+            for x, n in enumerate(row):
+                p = Point(x, y)
+                # 获取当前行已用的数
+                Row = self.getRowNums(y)
+
+                # 获取当前列已用的数
+                Col = self.getColNums(x)
+
+                # 排除所有已使用的数
+                range9 = [x for x in range(1, 10)]
+                for x in Row + Col:
+                    if x in range9:
+                        range9.remove(x)
+                p.available = range9
+                if len(range9) == 1:
+                    p.value = range9[0]
+                else:
+                    p.value = 0
+
+                # 存入数独酷
+                self.Sudoku[str(x)+str(y)] = p
+            return self.Sudoku
+
+    # 构建数独库
+    def constBoard(self):
+        self.board = [[0]*9 for i in range(9)]
+        for y, row in enumerate(board):
+            for x, n in enumerate(row):
+                if n is '.':
+                    self.board[y][x] = 0
+                else:
+                    self.board[y][x] = int(n)
+        return True
+
+    def constSudoku(self):
+        self.Sudoku  = [list(range(1, 10)) for x in range(1, 10)]
+        for y, row in enumerate([list(range(1, 10)) for x in range(1, 10)]):
+            for x, n in enumerate(row):
+                p = Point(x, y)
+                self.Sudoku[y][x] = p
+        return True
+
     def solveSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        self.board = board
-
         list_curb_dict = {
                             '00': {}, '01': {}, '02': {},
                             '10': {}, '11': {}, '12': {},
                             '20': {}, '21': {}, '22': {}}
-        for y, row in enumerate(self.Sudoku):
+
+        # 构建完毕则自我迭代一次
+        if self.constBoard() and self.constSudoku():
+            for y, row in enumerate(self.selfCount()):
+                for x, p in enumerate(row):
+                    if p.value > 0:
+                        self.board[x][y] = p.value
+
+        return True
+
+
+    # 测试专用
+    def test(self):
+        s  = [list(range(1, 10)) for x in range(1, 10)]
+        for y, row in enumerate([list(range(1, 10)) for x in range(1, 10)]):
             for x, n in enumerate(row):
                 p = Point(x, y)
-                # 获取当前行可用的数
-                RowAvailable = self.getRowAvailableNums(y)
-                # 获取当前列可用的数
-                ColAvailable = self.getColAvailableNums(x)
-                # 获取当前方阵可用的数
-                CubeAvailable = self.getCubeAvailableNums(x,y)
-                p.available = [[[range(9)].remove(n) for n in RowAvailable].remove(n) for x in ColAvailable]
-                if(len(p.available) == 1):
-                    p.value = p.available[0]
-                elif(len(p.available) > 1):
-                    p.value = 0
-        print(self.Sudoku)
-        return True
+                s[y][x] = p
+        print(s)
+
+
+
 
 
 board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
@@ -79,4 +131,5 @@ board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
          [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
 so = Solution()
-print(so.solveSudoku(board))
+so.test()
+# print(so.solveSudoku(board))
